a93f912fe5523829d977221a4a732cd21075b613
ensk26 <tjensk26@gmail.com>
2023-01-02 20:29:45 +0900

    feat: 개발 환경 구축

---
+++ src/main/java/com/backend/arthere/ArtHereApplication.java
@@ -0,0 +1,13 @@
+package com.backend.arthere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtHereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/arthere/ArtHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.arthere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
