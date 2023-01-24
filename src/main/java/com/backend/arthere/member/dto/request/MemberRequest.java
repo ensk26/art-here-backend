534cec5ed8743a65d86ee700ce5d7e60e4ae39f6
coucouluv <oy5325@gmail.com>
2023-01-24 15:10:59 +0900

    feat: 멤버 request dto 구현

---
+++ src/main/java/com/backend/arthere/member/dto/request/MemberRequest.java
@@ -0,0 +1,12 @@
+package com.backend.arthere.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberRequest {
+    private Long id;
+}
