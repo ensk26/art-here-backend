236d23d4db3abc3427c54f8b8536e1284901f698
coucouluv <oy5325@gmail.com>
2023-01-25 12:45:02 +0900

    feat: UserDetails 객체 구현

---
+++ src/main/java/com/backend/arthere/auth/domain/UserPrincipal.java
@@ -0,0 +1,84 @@
+package com.backend.arthere.auth.domain;
+
+import com.backend.arthere.member.domain.Member;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class UserPrincipal implements UserDetails {
+    private Long id;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
+
+    public UserPrincipal(Long id, String email, String password,
+                         Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.authorities = authorities;
+        this.password = password;
+    }
+
+    public static UserPrincipal create(Member member) {
+        List<GrantedAuthority> authorities = Collections
+                .singletonList(new SimpleGrantedAuthority(member.getRoleName()));
+        return new UserPrincipal(
+                member.getId(),
+                member.getEmail(),
+                null,
+                authorities
+        );
+    }
+
+    public static UserPrincipal create(Member member, Map<String, Object> attributes) {
+        UserPrincipal userPrincipal = UserPrincipal.create(member);
+        userPrincipal.setAttributes(attributes);
+        return userPrincipal;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
