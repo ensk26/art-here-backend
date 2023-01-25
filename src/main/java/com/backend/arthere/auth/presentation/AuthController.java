6fb5ade4d3868f4e5710530a4165e908ac541346
coucouluv <oy5325@gmail.com>
2023-01-25 22:44:29 +0900

    feat: auth controller 재발급, 로그아웃 요청 기능 구현

---
+++ src/main/java/com/backend/arthere/auth/presentation/AuthController.java
@@ -0,0 +1,34 @@
+package com.backend.arthere.auth.presentation;
+
+import com.backend.arthere.auth.application.AuthService;
+import com.backend.arthere.auth.dto.request.TokenRequest;
+import com.backend.arthere.auth.dto.response.TokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/token/reissue")
+    public ResponseEntity<?> reissue(@RequestBody @Valid final TokenRequest tokenRequest) {
+        TokenResponse tokenResponse = authService.reissue(tokenRequest.getRefreshToken());
+        return ResponseEntity.ok(tokenResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestBody @Valid final TokenRequest tokenRequest) {
+        authService.logout(tokenRequest.getRefreshToken());
+        return ResponseEntity.ok().build();
+    }
+
+
+}
