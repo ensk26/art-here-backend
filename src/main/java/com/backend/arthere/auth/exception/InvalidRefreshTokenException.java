7c2472586158052df3db308c40822b9d31b57ca0
coucouluv <oy5325@gmail.com>
2023-01-25 22:31:41 +0900

    feat: 유효하지 않는 리프레시 토큰 예외 처리

---
+++ src/main/java/com/backend/arthere/auth/exception/InvalidRefreshTokenException.java
@@ -0,0 +1,11 @@
+package com.backend.arthere.auth.exception;
+
+public class InvalidRefreshTokenException extends RuntimeException{
+    public InvalidRefreshTokenException(final String message) {
+        super(message);
+    }
+
+    public InvalidRefreshTokenException() {
+        this("유효하지 않은 리프레시 토큰입니다.");
+    }
+}
