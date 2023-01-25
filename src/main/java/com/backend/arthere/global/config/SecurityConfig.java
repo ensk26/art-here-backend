340406b449a77325c33c25c3c71ffb56074a8fc7
coucouluv <oy5325@gmail.com>
2023-01-25 12:46:59 +0900

    feat: jwt security 설정 추가

--- src/main/java/com/backend/arthere/global/config/SecurityConfig.java
+++ src/main/java/com/backend/arthere/global/config/SecurityConfig.java
@@ -1,21 +1,57 @@
 package com.backend.arthere.global.config;
 
+import com.backend.arthere.auth.application.CustomUserDetailsService;
+import com.backend.arthere.auth.domain.TokenRepository;
+import com.backend.arthere.auth.jwt.JwtAccessDeniedHandler;
+import com.backend.arthere.auth.jwt.JwtAuthenticationEntryPoint;
+import com.backend.arthere.auth.jwt.JwtTokenAuthenticationFilter;
+import com.backend.arthere.auth.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
 public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final TokenRepository tokenRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+        httpSecurity.httpBasic().disable()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and().formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeHttpRequests()
                 .antMatchers("/actuator/health").permitAll()
                 .antMatchers("/profile").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint);
 
+        httpSecurity.addFilterBefore(new JwtTokenAuthenticationFilter
+                        (jwtTokenProvider,customUserDetailsService, tokenRepository),
+                UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
