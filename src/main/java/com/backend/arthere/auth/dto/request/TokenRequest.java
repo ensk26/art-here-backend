7372de3841ca6838faa9080ee01dcb619b70a508
coucouluv <oy5325@gmail.com>
2023-01-29 23:04:04 +0900

    feat: 토큰 request Dto에 id 변수 추가

--- src/main/java/com/backend/arthere/auth/dto/request/TokenRequest.java
+++ src/main/java/com/backend/arthere/auth/dto/request/TokenRequest.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenRequest {
+
+    @NotNull(message = "아이디를 입력하세요.")
+    private Long id;
 
     @NotBlank(message = "리프레시 토큰을 입력하세요.")
     private String refreshToken;
