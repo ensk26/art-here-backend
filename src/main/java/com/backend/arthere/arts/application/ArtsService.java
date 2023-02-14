756e4c263f610bc82b3b130cf0d0de66089b0e67
ensk26 <tjensk26@gmail.com>
2023-02-14 16:05:13 +0900

    test: QueryDsl 테스트 코드 실행시 bean 등록을 위한 config 추가

--- src/main/java/com/backend/arthere/arts/application/ArtsService.java
+++ src/main/java/com/backend/arthere/arts/application/ArtsService.java
@@ -2,6 +2,7 @@ package com.backend.arthere.arts.application;
 
 import com.backend.arthere.arts.domain.ArtsRepository;
 import com.backend.arthere.arts.dto.ArtImageResponse;
+import com.backend.arthere.image.util.PresignedURLUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,22 @@ public class ArtsService {
 
     private final ArtsRepository artsRepository;
 
+    private final PresignedURLUtils presignedURLUtils;
+
     public List<ArtImageResponse> findArtImageByRevisionDate(Long offset, Long limit) {
 
-        return artsRepository.findArtImageByRevisionDate(offset, limit);
+        List<ArtImageResponse> artImageResponses = artsRepository.findArtImageByRevisionDate(offset, limit);
+        createImageSharePresignedURLByImageURL(artImageResponses);
+
+        return artImageResponses;
+    }
+
+    private void createImageSharePresignedURLByImageURL(List<ArtImageResponse> artImageResponses) {
+
+        for (ArtImageResponse artImageRespons : artImageResponses) {
+
+            String presignedURL = presignedURLUtils.createImageShareURL(artImageRespons.getImageURL());
+            artImageRespons.setImageURL(presignedURL);
+        }
     }
 }
