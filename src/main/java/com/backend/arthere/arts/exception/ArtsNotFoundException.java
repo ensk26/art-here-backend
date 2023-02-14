275aee0c5f28b3879fad533fe8ef1c138daaf46e
ensk26 <tjensk26@gmail.com>
2023-02-14 22:45:24 +0900

    feat: DB에서 요청한 offset과 limit에서 데이터가 없을때 ArtsNotFoundException 예외 처리

---
+++ src/main/java/com/backend/arthere/arts/exception/ArtsNotFoundException.java
@@ -0,0 +1,12 @@
+package com.backend.arthere.arts.exception;
+
+public class ArtsNotFoundException extends RuntimeException {
+
+    public ArtsNotFoundException(final String message) {
+        super(message);
+    }
+
+    public ArtsNotFoundException() {
+        this("해당 데이터가 존재하지 않습니다.");
+    }
+}
