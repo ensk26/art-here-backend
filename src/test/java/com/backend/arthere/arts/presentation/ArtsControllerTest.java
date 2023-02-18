1ed253d6712aad97a572fed7afcf70a616f7b0d5
ensk26 <tjensk26@gmail.com>
2023-02-18 23:19:12 +0900

    test: 중심 위치 이미지 반환 controller 테스트 추가

--- src/test/java/com/backend/arthere/arts/presentation/ArtsControllerTest.java
+++ src/test/java/com/backend/arthere/arts/presentation/ArtsControllerTest.java
@@ -1,6 +1,7 @@
 package com.backend.arthere.arts.presentation;
 
 import com.backend.arthere.arts.application.ArtsService;
+import com.backend.arthere.arts.dto.ArtImageByLocationResponse;
 import com.backend.arthere.arts.dto.ArtImageResponse;
 import com.backend.arthere.arts.exception.ArtsNotFoundException;
 import com.backend.arthere.global.BaseControllerTest;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -84,6 +86,61 @@ class ArtsControllerTest extends BaseControllerTest {
                 );
     }
 
+    @Test
+    @WithMockUser
+    void 지도화면_이미지_중심위치_지정반경_데이터_응답() throws Exception {
+
+        //given
+        List<ArtImageByLocationResponse> response = artsImageByLocationResponse();
+        given(artsService.findArtImageByLocation(anyDouble(), anyDouble())).willReturn(response);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/image/map")
+                .param("latitude", "37.587241")
+                .param("longitude", "127.019941"));
+
+        //then
+        resultActions.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(
+                        document("image/map",
+                                requestParameters(
+                                        parameterWithName("latitude").description("사용자 위치 위도"),
+                                        parameterWithName("longitude").description("사용자 위치 경도")
+                                ),
+                                responseFields(
+                                        fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("id"),
+                                        fieldWithPath("[].artName").type(JsonFieldType.STRING).description("작품 이름"),
+                                        fieldWithPath("[].imageURL").type(JsonFieldType.STRING).description("이미지 URL"),
+                                        fieldWithPath("[].latitude").type(JsonFieldType.NUMBER).description("위도"),
+                                        fieldWithPath("[].longitude").type(JsonFieldType.NUMBER).description("경도")
+                                )
+                        )
+                );
+    }
+
+    @Test
+    @WithMockUser
+    void 지도화면_이미지_중심위치_지정반경_데이터_없는_예외_응답() throws Exception {
+
+        //given
+        given(artsService.findArtImageByLocation(anyDouble(), anyDouble())).willThrow(new ArtsNotFoundException());
+
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/image/map")
+                .param("latitude", "37.587241")
+                .param("longitude", "127.019941"));
+
+        //then
+        resultActions.andExpect(status().isNotFound())
+                .andDo(print())
+                .andDo(
+                        document("image/map/notfound")
+                );
+    }
+
     private List<ArtImageResponse> artsImageResponse() {
 
         String artName = "모래작품";
@@ -93,6 +150,22 @@ class ArtsControllerTest extends BaseControllerTest {
         for (int i = 1; i < 6; i++) {
             responseList.add(new ArtImageResponse((long) i, artName + i,
                     imageURL + i + ".jpg?X-Amz-Algorithm"));
+        }
+
+        return responseList;
+    }
+
+    private List<ArtImageByLocationResponse> artsImageByLocationResponse() {
+
+        String artName = "모래작품";
+        String imageURL = "https://art-here-frontend.s3.ap-northeast-2.amazonaws.com/image/sand";
+        Double latitude = 37.58724;
+        Double longitude = 127.01994;
+        List<ArtImageByLocationResponse> responseList = new ArrayList<>();
+
+        for (int i = 1; i < 6; i++) {
+            responseList.add(new ArtImageByLocationResponse((long) i, artName + i,
+                    imageURL + i + ".jpg?X-Amz-Algorithm", latitude + i, longitude + i));
         }
 
         return responseList;
