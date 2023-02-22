475465fb4d768d6fe48fb62e3d9c92d6ecdeeb9e
ensk26 <tjensk26@gmail.com>
2023-02-22 23:49:59 +0900

    feat: 수정일 기준 응답 데이터, 다음 데이터 존재 여부, 다음 인덱스 날짜 반환 dto

---
+++ src/main/java/com/backend/arthere/arts/dto/ArtImageByRevisionDateResponse.java
@@ -0,0 +1,19 @@
+package com.backend.arthere.arts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArtImageByRevisionDateResponse {
+
+    private List<ArtImageResponse> artImageResponses;
+
+    private String next;
+
+    private Boolean hasNext;
+}
