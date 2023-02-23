31a29e0e79b3e76fa2c69657160c36475420b3f1
coucouluv <oy5325@gmail.com>
2023-02-23 23:35:05 +0900

    test: 수정 메서드 테스트 추가

--- src/test/java/com/backend/arthere/details/domain/DetailsTest.java
+++ src/test/java/com/backend/arthere/details/domain/DetailsTest.java
@@ -23,7 +23,73 @@ class DetailsTest {
 
         //then
         assertThat(details.isState()).isEqualTo(false);
+    }
+    
+    @Test
+    @DisplayName("작가 이름을 수정한다.")
+    public void 작가_이름_수정() throws Exception {
+        //given
+        String updateAuthorName = "작가 변경";
+        Details details = Details.builder()
+                .authorName("작가").build();
+        //when
+        details.updateAuthorName(updateAuthorName);
+        //then
+        assertThat(details.getAuthorName()).isEqualTo(updateAuthorName);
+    }
 
+    @Test
+    @DisplayName("담당 기관을 수정한다.")
+    public void 담당_기관_수정() throws Exception {
+        //given
+        String updateAgency = "담당 기관 변경";
+        Details details = Details.builder()
+                .agency("담당 기관").build();
+        //when
+        details.updateAgency(updateAgency);
+        //then
+        assertThat(details.getAgency()).isEqualTo(updateAgency);
+    }
+
+    @Test
+    @DisplayName("작품 상세 내용을 수정한다.")
+    public void 작품_상세_내용_수정() throws Exception {
+        //given
+        String updateInfo = "작품 상세 내용 수정";
+        Details details = Details.builder()
+                .info("상세 내용").build();
+        //when
+        details.updateInfo(updateInfo);
+        //then
+        assertThat(details.getInfo()).isEqualTo(updateInfo);
+    }
+
+    @Test
+    @DisplayName("작품 상태를 수정한다.")
+    public void 작품_상태_수정() throws Exception {
+        //given
+        Details details = Details.builder()
+                .state(false).build();
+        //when
+        details.updateState(true);
+        //then
+        assertThat(details.isState()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("작품 전시 기간을 수정한다.")
+    public void 작품_전시_기간_수정() throws Exception {
+        ExhibitionPeriod period = new ExhibitionPeriod(LocalDate.now(),
+                LocalDate.of(2025,01,01));
+        //given
+        Details details = Details.builder()
+                .period(new ExhibitionPeriod(LocalDate.of(2024,01,01), null))
+                .build();
+        //when
+        details.updatePeriod(period);
+        //then
+        assertThat(details.getPeriod().getStartDate()).isEqualTo(period.getStartDate());
+        assertThat(details.getPeriod().getEndDate()).isEqualTo(period.getEndDate());
     }
 
 }
