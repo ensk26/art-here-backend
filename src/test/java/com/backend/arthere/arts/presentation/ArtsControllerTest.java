452d638f451fa9be4759dc41ab9691462748c6e2
ensk26 <tjensk26@gmail.com>
2023-02-26 23:18:39 +0900

    test: 수정일 인덱스 적용 수정, validate 조건 추가로 테스트 추가

--- src/test/java/com/backend/arthere/arts/presentation/ArtsControllerTest.java
+++ src/test/java/com/backend/arthere/arts/presentation/ArtsControllerTest.java
@@ -1,9 +1,11 @@
 package com.backend.arthere.arts.presentation;
 
 import com.backend.arthere.arts.application.ArtsService;
+import com.backend.arthere.arts.dto.ArtImageByRevisionDateResponse;
 import com.backend.arthere.arts.dto.ArtImageResponse;
 import com.backend.arthere.arts.exception.ArtsNotFoundException;
 import com.backend.arthere.global.BaseControllerTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,7 +17,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -33,16 +36,17 @@ class ArtsControllerTest extends BaseControllerTest {
 
     @Test
     @WithMockUser
-    void 메인화면_이미지_수정일_내림차순_응답() throws Exception {
+    void 메인화면_이미지_수정일_내림차순_처음_응답() throws Exception {
 
         //given
-        List<ArtImageResponse> response = artsImageResponse();
-        given(artsService.findArtImageByRevisionDate(anyLong(), anyLong())).willReturn(response);
+        String next = "2023-02-26T00:09:47.019594";
+        boolean hasNext = true;
+        ArtImageByRevisionDateResponse response = artsImageResponse(next, hasNext);
+        given(artsService.findArtImageByRevisionDate(any(), anyInt())).willReturn(response);
 
         //when
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
                 .get("/api/image/media")
-                .param("offset", "1")
                 .param("limit", "5"));
 
         //then
@@ -51,13 +55,56 @@ class ArtsControllerTest extends BaseControllerTest {
                 .andDo(
                         document("image/media",
                                 requestParameters(
-                                        parameterWithName("offset").description("요청하는 데이터 시작 위치"),
                                         parameterWithName("limit").description("요청하는 데이터 개수")
                                 ),
                                 responseFields(
-                                        fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("id"),
-                                        fieldWithPath("[].artName").type(JsonFieldType.STRING).description("작품 이름"),
-                                        fieldWithPath("[].imageURL").type(JsonFieldType.STRING).description("이미지 URL")
+                                        fieldWithPath("artImageResponses.[].id").type(JsonFieldType.NUMBER)
+                                                .description("id"),
+                                        fieldWithPath("artImageResponses.[].artName").type(JsonFieldType.STRING)
+                                                .description("작품 이름"),
+                                        fieldWithPath("artImageResponses.[].imageURL").type(JsonFieldType.STRING)
+                                                .description("이미지 URL"),
+                                        fieldWithPath("next").type(JsonFieldType.STRING).description("다음 페이지 idx"),
+                                        fieldWithPath("hasNext").type(JsonFieldType.BOOLEAN).description("다음 페이지 유무")
+                                )
+                        )
+                );
+    }
+
+    @Test
+    @WithMockUser
+    void 메인화면_이미지_수정일_내림차순_다음_응답() throws Exception {
+
+        //given
+        String next = "2023-02-26T00:09:47.219594";
+        boolean hasNext = true;
+        ArtImageByRevisionDateResponse response = artsImageResponse(next, hasNext);
+        given(artsService.findArtImageByRevisionDate(any(), anyInt())).willReturn(response);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/image/media")
+                .param("idx", "2023-02-26T00:09:47.019594")
+                .param("limit", "5"));
+
+        //then
+        resultActions.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(
+                        document("image/media/next",
+                                requestParameters(
+                                        parameterWithName("idx").description("요청을 시작하는 위치"),
+                                        parameterWithName("limit").description("요청하는 데이터 개수")
+                                ),
+                                responseFields(
+                                        fieldWithPath("artImageResponses.[].id").type(JsonFieldType.NUMBER)
+                                                .description("id"),
+                                        fieldWithPath("artImageResponses.[].artName").type(JsonFieldType.STRING)
+                                                .description("작품 이름"),
+                                        fieldWithPath("artImageResponses.[].imageURL").type(JsonFieldType.STRING)
+                                                .description("이미지 URL"),
+                                        fieldWithPath("next").type(JsonFieldType.STRING).description("다음 페이지 idx"),
+                                        fieldWithPath("hasNext").type(JsonFieldType.BOOLEAN).description("다음 페이지 유무")
                                 )
                         )
                 );
@@ -68,12 +115,11 @@ class ArtsControllerTest extends BaseControllerTest {
     void 메인화면_이미지_수정일_내림차순_데이터_없는_예외_응답() throws Exception {
 
         //given
-        given(artsService.findArtImageByRevisionDate(anyLong(), anyLong())).willThrow(new ArtsNotFoundException());
+        given(artsService.findArtImageByRevisionDate(any(), anyInt())).willThrow(new ArtsNotFoundException());
 
         //when
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
                 .get("/api/image/media")
-                .param("offset", "1")
                 .param("limit", "5"));
 
         //then
@@ -84,7 +130,56 @@ class ArtsControllerTest extends BaseControllerTest {
                 );
     }
 
-    private List<ArtImageResponse> artsImageResponse() {
+    @Test
+    @WithMockUser
+    void 메인화면_3이미지_수정일_내림차순_요청_limit_1미만_예외_응답() throws Exception {
+
+        //when //then
+        Assertions.assertThatThrownBy(() ->
+                mockMvc.perform(MockMvcRequestBuilders
+                                .get("/api/image/media")
+                                .param("limit", "0"))
+                        .andExpect(status().isBadRequest())
+                        .andDo(print())
+                        .andDo(
+                                document("image/media/min")
+                        )).isInstanceOf(Exception.class);
+    }
+
+    @Test
+    @WithMockUser
+    void 메인화면_이미지_수정일_내림차순_요청_limit_10초과_예외_응답() throws Exception {
+
+        //when //then
+        Assertions.assertThatThrownBy(() ->
+                mockMvc.perform(MockMvcRequestBuilders
+                                .get("/api/image/media")
+                                .param("limit", "11"))
+                        .andExpect(status().isBadRequest())
+                        .andDo(print())
+                        .andDo(
+                                document("image/media/max")
+                        )).isInstanceOf(Exception.class);
+    }
+
+    @Test
+    @WithMockUser
+    void 메인화면_이미지_수정일_내림차순_요청_limit_null_예외_응답() throws Exception {
+
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/image/media")
+                .param("limit", ""));
+
+        //then
+        resultActions.andExpect(status().isBadRequest())
+                .andDo(print())
+                .andDo(
+                        document("image/media/null")
+                );
+    }
+
+    private ArtImageByRevisionDateResponse artsImageResponse(String next, boolean hasNext) {
 
         String artName = "모래작품";
         String imageURL = "https://art-here-frontend.s3.ap-northeast-2.amazonaws.com/image/sand";
@@ -95,6 +190,6 @@ class ArtsControllerTest extends BaseControllerTest {
                     imageURL + i + ".jpg?X-Amz-Algorithm"));
         }
 
-        return responseList;
+        return new ArtImageByRevisionDateResponse(responseList, next, hasNext);
     }
 }
