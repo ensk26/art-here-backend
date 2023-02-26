6a475bf3518ee85b8299fda6b3814ec055db52d6
ensk26 <tjensk26@gmail.com>
2023-02-27 02:06:18 +0900

    refactor: 매핑방식 변경, NotNull -> NotBlank로 변경

--- src/main/java/com/backend/arthere/arts/presentation/ArtsController.java
+++ src/main/java/com/backend/arthere/arts/presentation/ArtsController.java
@@ -2,7 +2,8 @@ package com.backend.arthere.arts.presentation;
 
 import com.backend.arthere.arts.application.ArtsService;
 import com.backend.arthere.arts.dto.ArtImageByLocationResponse;
-import com.backend.arthere.arts.dto.ArtImageResponse;
+import com.backend.arthere.arts.dto.ArtImageByRevisionDateRequest;
+import com.backend.arthere.arts.dto.ArtImageByRevisionDateResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,8 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Validated
@@ -24,16 +24,15 @@ public class ArtsController {
     private final ArtsService artsService;
 
     @GetMapping("/media")
-    public ResponseEntity<?> findArtImageByRevisionDate(@RequestParam("offset") @NotNull @Min(0) Long offset,
-                                                        @RequestParam("limit") @NotNull @Min(1) Long limit) {
+    public ResponseEntity<?> findArtImageByRevisionDate(ArtImageByRevisionDateRequest request) {
 
-        List<ArtImageResponse> artImageResponses = artsService.findArtImageByRevisionDate(offset, limit);
-        return ResponseEntity.ok(artImageResponses);
+        ArtImageByRevisionDateResponse responses = artsService.findArtImageByRevisionDate(request);
+        return ResponseEntity.ok(responses);
     }
 
     @GetMapping("/map")
-    public ResponseEntity<?> findArtImageByLocation(@RequestParam("latitude") @NotNull Double latitude,
-                                                    @RequestParam("longitude") @NotNull Double longitude) {
+    public ResponseEntity<?> findArtImageByLocation(@RequestParam("latitude") @NotBlank Double latitude,
+                                                    @RequestParam("longitude") @NotBlank Double longitude) {
 
         List<ArtImageByLocationResponse> artImageResponses = artsService.findArtImageByLocation(latitude, longitude);
         return ResponseEntity.ok(artImageResponses);
