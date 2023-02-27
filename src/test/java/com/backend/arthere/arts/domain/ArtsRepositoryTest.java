254d6d1498a14beb5cca6a41f0e9f97831da3e1b
ensk26 <tjensk26@gmail.com>
2023-02-27 23:48:29 +0900

    test: 수정일 기준 미만, 시간이 같을때 데이터 반환 테스트 추가

--- src/test/java/com/backend/arthere/arts/domain/ArtsRepositoryTest.java
+++ src/test/java/com/backend/arthere/arts/domain/ArtsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.backend.arthere.arts.domain;
 
+import com.backend.arthere.arts.dto.ArtImageByRevisionDateRequest;
 import com.backend.arthere.arts.dto.ArtImageResponse;
 import com.backend.arthere.global.config.JpaConfig;
 import com.backend.arthere.global.config.QueryDslConfig;
@@ -9,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 @DataJpaTest
 @Import({QueryDslConfig.class, JpaConfig.class})
@@ -22,16 +26,66 @@ class ArtsRepositoryTest {
     void 수정일_내림차순으로_id_작품이름_이미지URL_반환() {
 
         //given
-        Long offset = 0L;
-        Long limit = 5L;
+        ArtImageByRevisionDateRequest request = request(null, null, "20");
         artsSaveData();
-        ArtImageResponse response = new ArtImageResponse(1L, "모래작품1", "image/sand1");
 
         //when
-        List<ArtImageResponse> artImageResponses = artsRepository.findArtImageByRevisionDate(offset, limit);
+        List<ArtImageResponse> artImageResponses = artsRepository.findArtImageByRevisionDate(request);
 
         ///then
-        Assertions.assertThat(artImageResponses.get(4)).usingRecursiveComparison().isEqualTo(response);
+        Assertions.assertThat(artImageResponses.get(4).getArtName()).isEqualTo("모래작품1");
+    }
+
+    @Test
+    void 시작_수정일보다_미만인_데이터_반환() throws InterruptedException {
+
+        //given
+        artsSaveData();
+        sleep(100);
+        LocalDateTime now = LocalDateTime.now();
+        String dateIdx = now.toString();
+        ArtImageByRevisionDateRequest request = request("1", dateIdx, "5");
+
+        //when
+        List<ArtImageResponse> artImageResponses = artsRepository.findArtImageByRevisionDate(request);
+
+        ///then
+        Long id = artImageResponses.get(0).getId();
+        List<LocalDateTime> date = artsRepository.findRevisionDateById(id);
+        LocalDateTime next = date.get(0);
+
+        Assertions.assertThat(next).isBefore(now);
+    }
+
+    @Test
+    void 시작_수정일보다_미만이고_시간이_같은_데이터_조건_확인() {
+
+        //given
+        artsSaveData();
+        //artsSaveData();
+        ArtImageByRevisionDateRequest request = request(null, null, "1");
+
+        List<ArtImageResponse> artImageResponses = artsRepository.findArtImageByRevisionDate(request);
+
+        Long preId = artImageResponses.get(0).getId();
+        List<LocalDateTime> preDates = artsRepository.findRevisionDateById(preId);
+        LocalDateTime preDate = preDates.get(0);
+
+        ArtImageByRevisionDateRequest nextRequest = request(preId.toString(), preDate.toString(), "1");
+
+        //when
+        List<ArtImageResponse> responses = artsRepository.findArtImageByRevisionDate(nextRequest);
+
+        ///then
+        Long nextId = responses.get(0).getId();
+        List<LocalDateTime> nextDates = artsRepository.findRevisionDateById(nextId);
+        LocalDateTime nextDate = nextDates.get(0);
+
+        if (nextDate.isEqual(preDate)) {
+            Assertions.assertThat(nextId).isLessThan(preId);
+        } else {
+            Assertions.assertThat(nextDate).isBefore(preDate);
+        }
     }
 
     private void artsSaveData() {
@@ -45,5 +99,20 @@ class ArtsRepositoryTest {
             artsRepository.save(new Arts(artName + i, imageURL + i, location,
                     address, Category.PICTURE));
         }
+    }
+
+    private ArtImageByRevisionDateRequest request(String idx, String revisionDateIdx, String limit) {
+
+        ArtImageByRevisionDateRequest request = new ArtImageByRevisionDateRequest();
+
+        if (idx != null) {
+            request.setIdx(idx);
+        }
+        if (revisionDateIdx != null) {
+            request.setRevisionDateIdx(revisionDateIdx);
+        }
+        request.setLimit(limit);
+
+        return request;
     }
 }
