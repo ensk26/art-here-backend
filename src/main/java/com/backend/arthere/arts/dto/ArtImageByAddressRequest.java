db36c86e7edc0045b944014b92c196dd2298b05a
coucouluv <oy5325@gmail.com>
2023-03-05 18:03:52 +0900

    refactor: notblank, 길이제한 valid 추가

--- src/main/java/com/backend/arthere/arts/dto/ArtImageByAddressRequest.java
+++ src/main/java/com/backend/arthere/arts/dto/ArtImageByAddressRequest.java
@@ -3,6 +3,9 @@ package com.backend.arthere.arts.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Getter
@@ -11,8 +14,11 @@ public class ArtImageByAddressRequest implements Serializable {
 
     private Long idx;
 
+    @NotBlank(message = "검색어를 입력하세요.")
     private String query;
 
+    @Min(value = 1, message = "limit은 1 이상 10 이하입니다.")
+    @Max(value = 10, message = "limit은 1 이상 10 이하입니다.")
     private int limit;
 
     public void setIdx(final String idx) {
