2fee321e262473509f988aec0d710447e9547677
coucouluv <oy5325@gmail.com>
2023-03-19 17:02:26 +0900

    feat: MissingRequestHeaderException 예외 처리 추가

--- src/main/java/com/backend/arthere/global/error/ControllerAdvice.java
+++ src/main/java/com/backend/arthere/global/error/ControllerAdvice.java
@@ -2,7 +2,6 @@ package com.backend.arthere.global.error;
 
 import com.backend.arthere.arts.exception.ArtsNotFoundException;
 import com.backend.arthere.arts.exception.InvalidCategoryException;
-import com.backend.arthere.auth.exception.FailedTokenAuthenticationException;
 import com.backend.arthere.auth.exception.InvalidRefreshTokenException;
 import com.backend.arthere.auth.exception.InvalidTokenException;
 import com.backend.arthere.auth.exception.RefreshTokenNotFoundException;
@@ -11,15 +10,16 @@ import com.backend.arthere.member.exception.MemberNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
-    @ExceptionHandler({InvalidRefreshTokenException.class, FailedTokenAuthenticationException.class,
-            InvalidTokenException.class})
+    @ExceptionHandler({InvalidRefreshTokenException.class, InvalidTokenException.class})
     public ResponseEntity<ErrorResponse> handleInvalid(final RuntimeException error) {
         ErrorResponse errorResponse = new ErrorResponse(error.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
@@ -33,6 +33,12 @@ public class ControllerAdvice {
 
         ErrorResponse errorResponse = new ErrorResponse(sb.toString());
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler({MissingRequestHeaderException.class})
+    public ResponseEntity<ErrorResponse> handleHeader(final Exception error) {
+        ErrorResponse errorResponse = new ErrorResponse("요청 헤더가 존재하지 않습니다.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler({InvalidCategoryException.class})
