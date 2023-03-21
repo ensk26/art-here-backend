02b89f301f0deb250f70cbb158cf648aac0a81c4
ensk26 <tjensk26@gmail.com>
2023-03-21 23:45:00 +0900

    feat: 최대, 최소 검증 조건 추가

--- src/main/java/com/backend/arthere/arts/dto/ArtImageByRevisionDateRequest.java
+++ src/main/java/com/backend/arthere/arts/dto/ArtImageByRevisionDateRequest.java
@@ -1,11 +1,11 @@
 package com.backend.arthere.arts.dto;
 
-import com.backend.arthere.global.validation.Date;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -13,23 +13,25 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ArtImageByRevisionDateRequest implements Serializable {
 
-    private LocalDateTime revisionDateIdx;
+    private LocalDateTime date;
 
+    @Min(value=1, message = "idx는 1이상입니다.")
     private Long idx;
 
-    @Min(1)
-    @Max(10)
+    @Min(value = 1, message = "limit은 1 이상 10 이하입니다.")
+    @Max(value = 10, message = "limit은 1 이상 10 이하입니다.")
+    @NotNull(message = "limit는 필수 값 입니다.")
     private Integer limit;
 
-    public void setRevisionDateIdx(@Date String revisionDateIdx) {
-        this.revisionDateIdx = LocalDateTime.parse(revisionDateIdx);
+    public void setDate(final String date) {
+        this.date = LocalDateTime.parse(date);
     }
 
-    public void setIdx(String idx) {
+    public void setIdx(final String idx) {
         this.idx = Long.parseLong(idx);
     }
 
-    public void setLimit(String limit) {
+    public void setLimit(final String limit) {
         this.limit = Integer.parseInt(limit);
     }
 }
