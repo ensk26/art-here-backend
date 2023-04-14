669ba924e83596eb550cea064341e3d8313ea273
ensk26 <tjensk26@gmail.com>
2023-04-15 00:17:22 +0900

    feat: 만족도 service

---
+++ src/main/java/com/backend/arthere/satisfactions/application/SatisfactionsService.java
@@ -0,0 +1,20 @@
+package com.backend.arthere.satisfactions.application;
+
+import com.backend.arthere.satisfactions.dto.response.SatisfactionsResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SatisfactionsService {
+
+    //만족도 공유
+    public List<SatisfactionsResponse> findSatisfactions(Long id) {
+
+        return null;
+    }
+
+    //만족도 추가, 수정
+
+    //만족도 삭제
+}
