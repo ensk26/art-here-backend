3ba6ff0e2d3fa3bd549db746c8a310d469ffe47b
ensk26 <tjensk26@gmail.com>
2023-04-15 00:14:52 +0900

    feat: 만족도 총계수 응답 dto

---
+++ src/main/java/com/backend/arthere/satisfactions/dto/response/SatisfactionsResponse.java
@@ -0,0 +1,15 @@
+package com.backend.arthere.satisfactions.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SatisfactionsResponse {
+
+    private String satisfaction;
+
+    private int count;
+}
