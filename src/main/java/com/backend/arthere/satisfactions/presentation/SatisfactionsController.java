f06a8342f1f1957efdf14da6c870af42611ff0f1
ensk26 <tjensk26@gmail.com>
2023-04-15 00:13:35 +0900

    feat: 만족도 총 개수 반환 controller

---
+++ src/main/java/com/backend/arthere/satisfactions/presentation/SatisfactionsController.java
@@ -0,0 +1,36 @@
+package com.backend.arthere.satisfactions.presentation;
+
+import com.backend.arthere.arts.dto.ArtImageByRevisionDateRequest;
+import com.backend.arthere.arts.dto.ArtImageByRevisionDateResponse;
+import com.backend.arthere.satisfactions.application.SatisfactionsService;
+import com.backend.arthere.satisfactions.dto.response.SatisfactionsResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/satisfaction")
+@RequiredArgsConstructor
+public class SatisfactionsController {
+
+    private final SatisfactionsService satisfactionsService;
+
+    //만족도 공유
+    @GetMapping("/media")
+    public ResponseEntity<?> findSatisfactions(@Valid @Min(value = 1, message = "id는 1이상의 수 입니다.") Long id) {
+
+        List<SatisfactionsResponse> responses = satisfactionsService.findSatisfactions(id);
+        return ResponseEntity.ok(responses);
+    }
+
+    //만족도 추가, 수정
+
+    //만족도 삭제
+}
