03d3dff94b844957d58641c271eab67cc9e6ceb1
ensk26 <tjensk26@gmail.com>
2023-04-19 23:22:40 +0900

    feat: 만족도 분류별 개수 요청 service

--- src/main/java/com/backend/arthere/satisfactions/application/SatisfactionsService.java
+++ src/main/java/com/backend/arthere/satisfactions/application/SatisfactionsService.java
@@ -1,17 +1,22 @@
 package com.backend.arthere.satisfactions.application;
 
+import com.backend.arthere.satisfactions.domain.SatisfactionsRepository;
 import com.backend.arthere.satisfactions.dto.response.SatisfactionsResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class SatisfactionsService {
+
+    private final SatisfactionsRepository satisfactionsRepository;
 
     //만족도 공유
     public List<SatisfactionsResponse> findSatisfactions(Long id) {
 
-        return null;
+        return satisfactionsRepository.findSatisfactions(id);
     }
 
     //만족도 추가, 수정
