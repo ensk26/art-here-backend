0ff0bc3892832fcdf3194578bf5e543ca7ecfed0
coucouluv <oy5325@gmail.com>
2023-04-29 12:18:55 +0900

    feat: 좋아요 수 증가, 감소 쿼리 구현

--- src/main/java/com/backend/arthere/post/domain/PostRepository.java
+++ src/main/java/com/backend/arthere/post/domain/PostRepository.java
@@ -1,6 +1,7 @@
 package com.backend.arthere.post.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,4 +12,11 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select p from Post p join fetch p.member where p.id = :id")
     Optional<Post> findPostWithMember(@Param("id") Long id);
 
+    @Modifying(clearAutomatically = true)
+    @Query("update Post set likeCount = likeCount + 1 where id = :id")
+    void increaseLikeCount(@Param("id") Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Post set likeCount = likeCount - 1 where id = :id")
+    void decreaseLikeCount(@Param("id") Long id);
 }
