9b6586caa2ce95bc89b43dfc8eca2ef84d2ba6cd
coucouluv <oy5325@gmail.com>
2023-04-29 12:25:21 +0900

    test: AuthenticationArgumentResolver mockbean 추가

--- src/test/java/com/backend/arthere/global/BaseControllerTest.java
+++ src/test/java/com/backend/arthere/global/BaseControllerTest.java
@@ -1,8 +1,10 @@
 package com.backend.arthere.global;
 
+import com.backend.arthere.auth.presentation.AuthenticationArgumentResolver;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,4 +16,7 @@ public class BaseControllerTest {
 
     @Autowired
     protected MockMvc mockMvc;
+
+    @MockBean
+    protected AuthenticationArgumentResolver authenticationArgumentResolver;
 }
