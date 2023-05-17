c55f998d7424f476ca1f4bcc3f12856058dacf68
coucouluv <oy5325@gmail.com>
2023-05-17 23:32:27 +0900

    refactor: 위도, 경도 정보 추가

--- src/main/java/com/backend/arthere/details/dto/response/ArtResponse.java
+++ src/main/java/com/backend/arthere/details/dto/response/ArtResponse.java
@@ -1,5 +1,6 @@
 package com.backend.arthere.details.dto.response;
 
+import com.backend.arthere.arts.domain.Location;
 import com.backend.arthere.details.domain.Details;
 import com.backend.arthere.arts.domain.Arts;
 import lombok.Getter;
@@ -29,9 +30,11 @@ public class ArtResponse {
 
     private String category;
 
+    private Location location;
+
     private ArtResponse(final Long id, final String authorName, final String agency, final String info,
                        final boolean state, final LocalDate startDate, final LocalDate endDate,
-                       final String roadAddress, final String category) {
+                       final String roadAddress, final String category, final Location location) {
         this.id = id;
         this.authorName = authorName;
         this.agency = agency;
@@ -41,13 +44,14 @@ public class ArtResponse {
         this.endDate = endDate;
         this.roadAddress = roadAddress;
         this.category = category;
+        this.location = location;
     }
 
     public static ArtResponse of(final Details details, final Arts arts) {
         return new ArtResponse(details.getId(), details.getAuthorName(), details.getAgency(),
                 details.getInfo(), details.isState(), details.getPeriod().getStartDate(),
                 details.getPeriod().getEndDate(), arts.getAddress().getRoadAddress(),
-                arts.getCategory().getCategoryName());
+                arts.getCategory().getCategoryName(), arts.getLocation());
     }
 
 }
