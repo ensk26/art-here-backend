e77de62c7b408ecef8aabf48fa4a5f4d624a2660
coucouluv <oy5325@gmail.com>
2023-05-19 14:38:37 +0900

    style: 줄바꿈 추가

--- src/main/java/com/backend/arthere/auth/domain/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/com/backend/arthere/auth/domain/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -40,7 +40,8 @@ public class HttpCookieOAuth2AuthorizationRequestRepository
     }
 
     @Override
-    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request,
+                                         HttpServletResponse response) {
         if(authorizationRequest == null) {
             CookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
             CookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
