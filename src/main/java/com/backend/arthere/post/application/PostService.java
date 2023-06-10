08a4d0bde954f851039aabcaf50d1e26e61ded9f
ensk26 <tjensk26@gmail.com>
2023-06-11 01:45:14 +0900

    feat: post 수정 service

--- src/main/java/com/backend/arthere/post/application/PostService.java
+++ src/main/java/com/backend/arthere/post/application/PostService.java
@@ -10,18 +10,22 @@ import com.backend.arthere.like.domain.LikeRepository;
 import com.backend.arthere.member.domain.Member;
 import com.backend.arthere.member.domain.MemberRepository;
 import com.backend.arthere.member.exception.MemberNotFoundException;
+import com.backend.arthere.member.exception.NotWriterException;
 import com.backend.arthere.post.domain.Post;
 import com.backend.arthere.post.domain.PostRepository;
+import com.backend.arthere.post.dto.request.AddPostsRequest;
+import com.backend.arthere.post.dto.request.CreatePostsRequest;
 import com.backend.arthere.post.dto.response.PostInfoResponse;
 import com.backend.arthere.post.dto.response.PostResponse;
 import com.backend.arthere.post.dto.response.PostsResponse;
-import com.backend.arthere.post.dto.resquest.CreatePostsRequest;
 import com.backend.arthere.post.exception.PostNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -41,6 +45,8 @@ public class PostService {
     private final AmazonS3 userS3Client;
 
     private final String userBucketName;
+
+    private final EntityManager em;
 
     @Transactional(readOnly = true)
     public PostResponse find(final Long id, final Long memberId) {
@@ -103,6 +109,26 @@ public class PostService {
             return artsRepository.getReferenceById(id);
         } catch (Exception e) {
             throw new ArtsNotFoundException();
+        }
+    }
+
+    @Transactional
+    public void addPost(AddPostsRequest request, Long userId) throws Exception {
+        checkWriter(request.getPostId(), userId);
+
+        Post post = em.find(Post.class, request.getPostId());
+        post.updateTitle(request.getTitle());
+        post.updateContent(request.getContent());
+        post.updateImageURL(request.getImageURL());
+    }
+
+    private void checkWriter(Long postId, Long userId) {
+        Optional<Post> post = postRepository.findById(postId);
+        if (post.isEmpty()) {
+            throw new PostNotFoundException();
+        }
+        if (!post.get().getMember().getId().equals(userId)) {
+            throw new NotWriterException();
         }
     }
 }
